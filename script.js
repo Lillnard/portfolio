5bdcaca93c222e9730358640a2cc211172f4cc84
Lillnard <lfonseca333@gmail.com>
2024-01-11 23:27:46 -0300

    alterações de cores e adição de imagens e links na section projetos

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//função modo dark
 
 let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
@@ -8,20 +9,25 @@ trilho.addEventListener('click',()=>{
 
 })
 
+//função visualizar habilidaddes
 
 const visualizar = new IntersectionObserver((avistado)=>{
     avistado.forEach((visto)=>{
         if(visto.isIntersecting){
-            visto.target.classList.add('show')
-        } else{
-            visto.target.classList.remove('show')
+            setTimeout(() => {
+                visto.target.classList.add('show');
+            }, 250); 
+        } else {
+            visto.target.classList.remove('show');
         }
-    })
-})
+    });
+});
 
 const section = document.querySelectorAll('.hidden')
 
 section.forEach((elemento)=> visualizar.observe(elemento))
+
+//função menu selecionado no scroll
 
 const links = document.querySelectorAll('.js')
 const sections = document.querySelectorAll('.section')
@@ -43,10 +49,12 @@ window.addEventListener('scroll', ()=>{
     })
 })
 
-
+//voltar ao home ao recarregar a pagina//
 window.onload = function() {
     window.scrollTo(0, 0);
 }
+
+//função para menu mobile abiri e fechar com toque em qualquer pate da tela
 
 let abrirMenu = document.getElementById('abrir-menu')
 let menu = document.getElementById('menu-mobile')
@@ -63,3 +71,17 @@ menu.addEventListener('click', ()=>{
 sombreado.addEventListener('click', ()=>{
     menu.classList.remove('menu-aberto')
 })
+
+
+//função abertura de modal de habilidades
+
+// function abrirModal(){
+//     const modal = document.getElementById('janela')
+//     modal.classList.add('abrir');
+
+//     modal.addEventListener('click', (e) =>{
+//         if(e.target.id == 'fehcar-modal' || e.target.id == 'modal'){
+//             modal.classList.remove('abrir')
+//         }
+//     })
+// }
